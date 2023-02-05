88360553401c618aaa01ea8a3cf334c531e707a6
mridini7 <105174159+mridini7@users.noreply.github.com>
2023-02-05 15:02:07 -0800

    Add files via upload

---
+++ screens/jokeScreen.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default class JokeScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text style={{ fontSize:15}}>Student 1: Maggie's Attendance</Text>
+        <TouchableOpacity
+              style={styles.buttons}
+              onPress={() => this.props.navigation.navigate('HomeScreen')}>
+              <Text style={{ fontSize:20, color:"green"}}>Present</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+              style={styles.buttons}
+              onPress={() => this.props.navigation.navigate('HomeScreen')}>
+              <Text style={{ fontSize:20, color:"red"}}>Absent</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100,
+  },
+    buttons: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 2,
+    borderRadius: 15,
+    margin: 10,
+    width: 160,
+    height: 50,
+  }
+});
